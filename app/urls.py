5a4665b4acbecc0ab180099f698de37728f780c5
Yamy <yamilagorrin@gmail.com>
2021-08-24 18:42:24 -0400

    TV-Show Crud

--- app/urls.py
+++ app/urls.py
@@ -4,9 +4,14 @@ urlpatterns = [
     path('', views.index),
     path('shows', views.shows),
     path('shows/new', views.shows_new),
-    path('shows_crear', views.shows_crear),
+    path('show_agregar', views.show_agregar),
+    path('shows/<int:id>', views.shows_mostrar),
+    
     path('shows/<int:id>/edit', views.shows_edit),
-    path('shows/<int:id>', views.shows_id),
+    path('shows/<int:id>/delete', views.shows_delete),
+    
+    
+    path('shows/edit', views.edit),
 
 
 ]
--- app/views.py
+++ app/views.py
@@ -1,23 +1,34 @@
 from django.shortcuts import render, redirect
 from .models import *
+# Import Datetime class
+from datetime import datetime
 
 def index(request):
     return redirect('/shows')
 
 def shows(request):
     context = {
-        'saludo': 'shows'
+        'shows': Show.objects.all()
     }
     return render(request, 'shows.html', context)
 
-def shows_new(request):
 
+def shows_new(request):
     context = {
         'saludo': 'Show create'
     }
     return render(request, 'shows_create.html', context)
 
-def shows_crear(request):
+def shows_mostrar(request, id):
+    show_by_id = Show.objects.get(id=id)
+    print("Mostrando: ", show_by_id)
+    context = {
+        'show' : show_by_id,
+    }
+    return render(request, 'shows_mostrar.html', context)
+
+
+def show_agregar(request):
     if request.method =='POST':
         print("---POST ----", request.POST)
         show = Show.objects.create(
@@ -26,17 +37,34 @@ def shows_crear(request):
             release_date = request.POST['release_date'],
             desc = request.POST['desc'],
         )
-    return redirect('/shows')
-    
+    return redirect("/shows/" + str(show.id))
+
 
 def shows_edit(request, id):
-    context = {
-        'saludo': 'Show update'
-    }
-    return render(request, 'shows_update.html', context)
+    if request.method == 'GET':
+        show_by_id = Show.objects.get(id=id)
+        print("Mostardo Show antes de editar: ", show_by_id)
+        context = {
+            'show': show_by_id,
+        }
+        return render(request, 'shows_edit.html', context)
 
-def shows_id(request,id):
-    context = {
-        'saludo': 'Show by id'
-    }
-    return render(request, 'shows_id.html', context)
+def edit(request):
+    print(request.POST)
+    if request.method == 'POST':
+        print(request.POST)
+        id_show = request.POST['id_show']
+        show = Show.objects.get(id=id_show)
+        print("Vamos a actualizar")
+        show.title = request.POST['title']
+        show.network = request.POST['network']
+        show.desc = request.POST['desc']
+        show.save()
+        return redirect(f'/shows/{id_show}')
+
+
+def shows_delete(request, id):
+    show_by_id = Show.objects.get(id=id)
+    print("Borrando: ", show_by_id)
+    show_by_id.delete()
+    return redirect('/shows')
